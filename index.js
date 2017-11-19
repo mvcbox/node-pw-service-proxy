e735e9b5bd15a2ed8953359b382ef046fd386c9e
mvcbox <mvcbox.org@gmail.com>
2017-11-19 18:22:59 +0200

    Update to 2.0.0

--- index.js
+++ index.js
@@ -1,6 +1,171 @@
 'use strict';
 
-module.exports = {
-    PwServiceProxy: require('./src/pw-proxy'),
-    handlers: require('./src/handlers')
-};
+const PwBuffer = require('pw-buffer');
+const net = require('net');
+const through2 = require('through2');
+const async = require('async');
+
+class PwServiceProxy
+{
+    /**
+     * @param {Object} options
+     */
+    constructor(options) {
+        this._options = Object.assign({}, {
+            bufferSize: 10485760,
+            bufferFreeSpaceGc: 1048576
+        }, options || {});
+        this._clientHandlers = [];
+        this._serverHandlers = [];
+    }
+
+    /**
+     * @param {Array} handlers
+     * @param {Stream} input
+     * @param {Stream} output
+     * @return {Stream}
+     * @private
+     */
+    _createHandlersStream(handlers, input, output) {
+        return through2.obj(function (packet, enc, streamDone) {
+            let _thisStream = this;
+
+            async.each(handlers, function (handler, next) {
+                if (
+                    handler.only && handler.only.indexOf(packet.opcode) === -1 ||
+                    handler.except && handler.except.indexOf(packet.opcode) !== -1
+                ) {
+                    return next();
+                }
+
+                handler.handler(packet, input, output, next);
+            }, function (err) {
+                packet.payload.writeCUInt(packet.payload.length, true).writeCUInt(packet.opcode, true);
+                _thisStream.push(packet.payload.buffer);
+                streamDone();
+            });
+        });
+    }
+
+    /**
+     * @return {Stream}
+     * @private
+     */
+    _createReadPacketStream() {
+        let buffer = new PwBuffer({
+            maxBufferLength: this._options.bufferSize
+        });
+        let packet;
+        let oldPointer;
+        let _this = this;
+
+        return through2.obj(function (chunk, enc, done) {
+            if (buffer.getFreeSpace() < _this._options.bufferFreeSpaceGc) {
+                buffer.gc();
+            }
+
+            buffer._writeNativeBuffer(chunk, false);
+
+            while (true) {
+                packet = {};
+                oldPointer = buffer.pointer;
+
+                if (!buffer.isReadableCUInt()) {
+                    buffer.pointer = oldPointer;
+                    break;
+                }
+
+                packet.opcode = buffer.readCUInt();
+
+                if (!buffer.isReadableCUInt()) {
+                    buffer.pointer = oldPointer;
+                    break;
+                }
+
+                packet.length = buffer.readCUInt();
+
+                if (!buffer.isReadable(packet.length)) {
+                    buffer.pointer = oldPointer;
+                    break;
+                }
+
+                packet.payload = buffer.readBuffer(packet.length, false, {
+                    maxBufferLength: packet.length + 10
+                });
+
+                this.push(packet);
+            }
+
+            done();
+        });
+    }
+
+    /**
+     * @param {Object} options
+     * @return {PwServiceProxy}
+     */
+    start(options) {
+        let _this = this;
+
+        net.createServer(function (clientSocket) {
+            let remoteAddr = clientSocket.remoteAddress + ':' + clientSocket.remotePort;
+            let alreadyClosed = false;
+            let serverSocket = net.createConnection(options.connect);
+
+            clientSocket
+                .pipe(_this._createReadPacketStream())
+                .pipe(_this._createHandlersStream(_this._clientHandlers, clientSocket, serverSocket))
+                .pipe(serverSocket)
+                .pipe(_this._createReadPacketStream())
+                .pipe(_this._createHandlersStream(_this._serverHandlers, serverSocket, clientSocket))
+                .pipe(clientSocket);
+
+            function closeConnection() {
+                if (alreadyClosed) {
+                    return;
+                }
+
+                alreadyClosed = true;
+                clientSocket.destroy().unref();
+                serverSocket.destroy().unref();
+                console.info('---------------------------------------------------------------------------');
+                console.info('[' + new Date().toLocaleString() + ']: Client disconnected [' + remoteAddr + ']');
+            }
+
+            serverSocket.on('close', closeConnection).on('error', closeConnection).setNoDelay(true);
+            clientSocket.on('close', closeConnection).on('error', closeConnection).setNoDelay(true);
+            console.info('---------------------------------------------------------------------------');
+            console.info('[' + new Date().toLocaleString() + ']: Client connected [' + remoteAddr + ']');
+        }).listen(options.listen, function () {
+            console.info('---------------------------------------------------------------------------');
+            console.info('[' + new Date().toLocaleString() + ']: Proxy start');
+        }).on('error', function (err) {
+            console.info('---------------------------------------------------------------------------');
+            console.error('[' + new Date().toLocaleString() + ']: Proxy error');
+            console.error(JSON.stringify(options, null, 2));
+            console.error(err.stack);
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Array} handlers
+     * @return {PwServiceProxy}
+     */
+    setClientHandlers(handlers) {
+        this._clientHandlers = handlers;
+        return this;
+    }
+
+    /**
+     * @param {Array} handlers
+     * @return {PwServiceProxy}
+     */
+    setServerHandlers(handlers) {
+        this._serverHandlers = handlers;
+        return this;
+    }
+}
+
+module.exports = PwServiceProxy;
